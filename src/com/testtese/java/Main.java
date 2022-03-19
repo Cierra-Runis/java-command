41d715a89ba48c3a0fbf6ccbe99cc6bf4d52dd64
Cierra-Runis <2864283875@qq.com>
2022-03-19 17:02:20 +0800

    完善输入指令提示

--- src/com/testtese/java/Main.java
+++ src/com/testtese/java/Main.java
@@ -8,13 +8,13 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter command just like /help\n");        //提示
+        System.out.print("Enter command just like /help\n> ");      //提示
         String input = scanner.nextLine();                          //第一条命令
         CommandForMatrix command = new CommandForMatrix(input);     //创建一个 CommandForMatrix 的对象
 
         while (!input.equals("/exit")) {                            //进入循环，只要输入不是 /exit 就不结束
             command.match(input);                                   //利用 匹配指令 方法处理命令
-            System.out.print("Enter command just like /help\n");    //提示
+            System.out.print("Enter command just like /help\n> ");  //提示
             input = scanner.nextLine();                             //再输入
         }
     }
